5063b9a2d7a2e724ca24895dcee9027f94f45032
tooriskygh <31419224-tooriskygh@users.noreply.replit.com>
2024-01-20 12:38:29 +0000

    updated corrections

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import yagmail_outlook
+import yagmail
 
 sender = 'emailautotesting8@gmail.com'
 receiver = 'fahalykaheh2@10mail.xyz'
@@ -8,9 +8,7 @@ subject = """Automation Testing"""
 
 contents = """This is a test email sent from Python"""
 
-password = os.getenv('PASSWORD')
-
-yag = yagmail_outlook.SMTP(user=sender, password=password)
+yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
 yag.send(to=receiver, subject=subject, contents=contents)
 print("Email Sent!")
 
